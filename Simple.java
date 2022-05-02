63031235d5ad235b9d537c263265d49924a20ed9
Sevitha <sevithak03@gmail.com>
2022-05-02 19:48:22 +0530

    Simple.java

---
+++ Simple.java
@@ -0,0 +1,18 @@
+    import javax.swing.*;  
+    public class Simple extends JFrame{  
+    Simple(){  
+             /* 
+    JButton b=new JButton("click");//creating instance of JButton  
+    b.setBounds(130,100,100, 40);  
+              
+    f.add(b);*///adding button in JFrame  
+              
+    setSize(400,500);//400 width and 500 height  
+    setLayout(null);//using no layout managers  
+    setVisible(true);//making the frame visible  
+    }  
+      
+    public static void main(String[] args) {  
+    new Simple();  
+    }  
+    }  
